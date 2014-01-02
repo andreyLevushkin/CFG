57fe3e7217e4d47c3a72132e100b96a9404b4f5f
Andrey Levushkin <andrey@whereismydot.com>
2014-01-02 14:32:03 +0000

    Initial import

---
+++ CFG.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE DeriveFunctor #-}
+module CFG where
+
+import Data.Function
+import Data.Map as M (Map, findWithDefault, fromList, alter, lookup) 
+import Data.Maybe 
+import Debug.Trace
+import Control.DeepSeq
+
+data CFG a b = CFG [(a,b)] [(a, (a, a))] a deriving (Show, Eq)
+
+data Parse a b = Lit Int b a 
+               | Prod a (Parse a b) (Parse a b)
+               deriving (Show, Eq, Functor)
+
+instance (NFData a, NFData b) => NFData (Parse a b) where
+    rnf = const () . fmap rnf
+
+instance NFData TL where
+    rnf v = v `seq` ()
+
+headVal :: Parse a b -> a
+headVal (Lit _ b val)  = val
+headVal (Prod val _ _) = val
+
+data TL = A | B | C | D | S deriving (Show, Eq, Bounded, Enum)
+
+testGrammer :: CFG TL Char
+testGrammer = CFG lit comp S
+    
+lit = [(A, 'a'), (B, 'b'), (A,'b')]
+comp = [(C, (B,B)), (D, (A,A)), (S, (C,D)), (A, (A,A)), (S, (A,A))]
+
+testString :: String 
+testString = "bbaa"        
+
+type Board a b = Map (Int, Int) [Parse a b]
+
+cell :: (Int, Int) -> Board a b -> [Parse a b]
+cell = findWithDefault [] 
+
+makeEdge :: Eq b => [(a, b)] -> [b] -> Board a b
+makeEdge rules = fromList . zip idx . map (matchLiteral rules) . zip [0..]
+    where
+        idx = [(i,i)| i <- [0..]]
+
+parse ::  (Show a, Eq a, Eq b, Show b) => CFG a b -> [b] -> [Parse a b]
+parse cfg@(CFG _ _ s) input = filter isTerminated . cell (n-1,0) $ buildBoard cfg input
+    where
+        isTerminated = (==s) . headVal
+        n            = length input
+
+
+buildBoard :: (Show a, Eq a, Eq b, Show b) => CFG a b -> [b] -> Board a b
+buildBoard (CFG lit comp stop) input 
+    = foldl (debug) edge [(i,j,k) | i <- [1 .. n-1], 
+                                    j <- [i-1,i-2  .. 0], 
+                                    k <- [1.. partitions i j]]
+    where
+        edge = makeEdge lit input
+        n    = length input
+        debug accum x  = (updateCell comp accum x) 
+
+partitions :: Int -> Int -> Int
+partitions i j | i - j < 2 = 1
+               | otherwise = i - j
+
+updateCell :: (Show a, Eq a, Show b)=> [(a, (a, a))] -> Board a b -> (Int, Int, Int)  -> Board a b
+updateCell rules board (i,j,k) = trace debug $ alter go (i, j) board
+    where
+        go (Just old) = Just $ old ++ productCell rules left right
+        go Nothing    = Just $ productCell rules left right
+        left  = cell l_idx board
+        right = cell r_idx board
+        l_idx = (i,j+k)
+        r_idx = (i-k, j)
+        debug = "{" ++ show i ++ " " ++ show j ++ " " ++ show k ++ "} :: { " ++ show l_idx ++ " " ++ show r_idx ++ "} => "  ++ show left ++ " -- " ++ show right
+
+
+matchLiteral :: Eq b => [(a, b)] -> (Int, b) ->  [Parse a b]
+matchLiteral rules (i,lit) =  map (Lit i lit . fst) . filter ((==lit) . snd) $ rules
+
+productCell :: (Show a, Eq a) => [(a, (a, a))] -> [Parse a b] -> [Parse a b] -> [Parse a b]
+productCell rules left right = concatMap (match rules) [(l,r)| l <- left, r <- right]
+    where 
+        --debug = "{" ++ show left ++ " " ++ show right ++ "}"
+
+match :: Eq a => [(a, (a, a))] -> (Parse a b, Parse a b) -> [Parse a b]
+match rules (p1, p2) =  catMaybes $ map f rules 
+    where 
+        f (v, (l, r)) | headVal p1 == l && headVal p2 == r = Just $ Prod v p1 p2
+                      | otherwise                          = Nothing
+
+x = [(i,j,k) | i <- [0..5], 
+               j <- reverse [0..2], 
+               k <- [1..(i)]]
